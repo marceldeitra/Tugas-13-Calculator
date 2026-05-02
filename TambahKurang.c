76642b5801df2485d51cd97c0d14901f8998ce5f
marceldeitra <marceldeitra@gmail.com>
2026-05-02 15:56:02 +0700

    final

--- TambahKurang.c
+++ TambahKurang.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-float add(int a, int b) {
+float add(float a, float b) {
     return a + b;
 }
 
-float sub(int a, int b) {
+float sub(float a, float b) {
     return a - b;
 }
 
 int main() {
-    int pilihan, a, b;
-    float hasil;
+    int pilihan;
+    float a, b, hasil;
 
     printf("Kalkulator\n");
     printf("1. Penambahan\n");
@@ -34,7 +34,7 @@ int main() {
             printf("Hasil: %.2f\n", hasil);
             break;
         default:
-            printf("Pilihan tidak valid.\n");
+            printf("Pilihannya tidak valid.\n");
     }
 
     return 0;
